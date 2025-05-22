bb5c1a1c0660443a55214dee56f2e39d7e663f04
Najmaldin Ishag <najmaldinosman@gmail.com>
2025-05-23 01:20:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-"strict";
-
 const nav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 
@@ -7,7 +5,9 @@ navToggle.addEventListener("click", () => {
   const visiblity = nav.getAttribute("data-visible");
   if (visiblity === "false") {
     nav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
   } else {
     nav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
   }
 });
